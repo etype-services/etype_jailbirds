4303755f1442115dda04be11f6a9f55a6839b691
Charlie Llewellin <charlie@llewellin.net>
2018-04-07 10:18:14 -0500

    investigate import errors

---
+++ cron.php
@@ -0,0 +1,22 @@
+#!/bin/env php
+
+<?php
+// define static var
+define('DRUPAL_ROOT', getcwd());
+
+// include bootstrap
+include_once('./includes/bootstrap.inc');
+
+// initialize stuff
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+//laod user
+global $user;
+
+if ($user->uid > 0) {
+  // user logged in
+  echo 'Welcome ' . $user->name . '!';
+} else {
+  // anonym user
+  echo 'Welcome guest!';
+}
